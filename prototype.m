ede20a30f9530bdc5f69cb4e27e20ec6e191ef86
soareswallace <wssj2@cin.ufpe.br>
2017-10-29 15:54:54 -0300

    Great Prototype

---
+++ prototype.m
@@ -0,0 +1,21 @@
+recObj = audiorecorder(44100, 16, 1);
+disp('Start speaking.')
+recordblocking(recObj, 5);
+disp('End of Recording.');
+myRecording = getaudiodata(recObj);
+interval = 0:0.25:10;
+exp_decay = exp(-interval);
+echo_filter = [0,8*exp_decay zeros(1, 22050) 0,6*exp_decay zeros(1, 22050) 0,4*exp_decay zeros(1, 22050) 0,2*exp_decay zeros(1, 22050) 0,1*exp_decay zeros(1, 22050)];
+
+y=conv(myRecording, echo_filter);
+
+sound(y, 44100, 16);
+figure(1);
+plot(myRecording);
+figure(2);
+plot(y);
+figure(3)
+plot(exp_decay);
+
+figure(4)
+plot(echo_filter);
--- voice_recorde.m
+++ voice_recorde.m
@@ -2,14 +2,12 @@
 %audiorecorder(Fs,nBits,nChannels) 
 %sets the sample rate Fs (in Hz), the sample size nBits, and the number of channels nChannels
 %creates an 8000 Hz, 8-bit, 1-channel audiorecorder object.
-recObj = audiorecorder;
+%if we divide the length of the audio by the record sample rate, that is
+%8000 we will get the durati=on of the recording.
+recObj = audiorecorder(44100, 16, 2);
 disp('Start speaking.')
-recordblocking(recObj, 5);
+recordblocking(recObj, 10);
+play(recObj);
 disp('End of Recording.');
 myRecording = getaudiodata(recObj);
 plot(myRecording);
-%if we divide the length of the audio by the record sample rate, that is
-%8000 we will get the duration of the recording.
-length(myRecording)/8000;
-segment = myRecording(8000*2 : 8000*4);
-plot(segment);
