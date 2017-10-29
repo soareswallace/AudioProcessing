2f90422f098765ff0a1e41a4e6f883f10eec88df
Wallace Soares <soareswallace@icloud.com>
2017-10-29 19:55:27 -0300

    Update prototype.m

--- prototype.m
+++ prototype.m
@@ -8,7 +8,7 @@ interval = 0:0.25:10;
 exp_decay = exp(-interval);
 echo_filter = [0,8*exp_decay zeros(1, 22050) 0,6*exp_decay zeros(1, 22050) 0,4*exp_decay zeros(1, 22050) 0,3*exp_decay zeros(1, 22050) 0,2*exp_decay zeros(1, 22050) 0,1*exp_decay zeros(1, 22050)];
 myRecording = transpose(myRecording);
-y = myConv(myRecording, echo_filter);
+y = conv(myRecording, echo_filter);
 
 sound(y, 44100, 16);
 figure(1);
