2ce181cf09cf5b8a78337eaf1982826ca48f2d5e
soareswallace <wssj2@cin.ufpe.br>
2017-10-29 18:42:32 -0300

    Good prototype

--- convo.m
+++ convo.m
@@ -6,7 +6,7 @@ function y = convo(u, v)
   
   for k = 1:tam
     for j = max(1, k + 1 - n):1:min(k, m)
-      w(k) += u(j) * v(k - j + 1);
+      w(k) = w(k) + u(j) * v(k - j + 1);
     end
   end
   y = w;
