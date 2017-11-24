a8961cc0270bb98a8a075710fd169fae08def435
soareswallace <wssj2@cin.ufpe.br>
2017-11-23 22:47:56 -0300

    Better Echoe

--- prototype.m
+++ prototype.m
@@ -4,9 +4,9 @@ disp('Start speaking.')
 recordblocking(recObj, 5);
 disp('End of Recording.');
 myRecording = getaudiodata(recObj);
-interval = 0:0.25:10;
+interval = 0:0.10:10;
 exp_decay = exp(-interval);
-echo_filter = [0,8*exp_decay zeros(1, 22050) 0,6*exp_decay zeros(1, 22050) 0,4*exp_decay zeros(1, 22050) 0,3*exp_decay zeros(1, 22050) 0,2*exp_decay zeros(1, 22050) 0,1*exp_decay zeros(1, 22050)];
+echo_filter = [0,8*exp_decay zeros(1, 9000) 0,6*exp_decay zeros(1, 9000) 0,4*exp_decay zeros(1, 9000) 0,3*exp_decay zeros(1, 9000) 0,2*exp_decay zeros(1,9000) 0,1*exp_decay zeros(1, 9000)];
 myRecording = transpose(myRecording);
 y = conv(myRecording, echo_filter);
 
@@ -17,3 +17,5 @@ figure(2);
 plot(y);
 figure(3)
 plot(echo_filter);
+figure(4);
+plot(exp_decay);
